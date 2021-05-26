085523ad03bebb9e19cdc78557c83e07bd6709b0
Wonyeong Choi <coma01155@gmail.com>
2021-05-26 19:38:50 +0900

    add calendar view

---
+++ app/src/main/java/com/example/teamproject/CalendarActivity.kt
@@ -0,0 +1,15 @@
+package com.example.teamproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+        init()
+    }
+    fun init(){
+
+    }
+}
