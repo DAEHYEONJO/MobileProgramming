33e02e99dc86610c5e47452d5ec3d285f9c7b9e3
IslandofDream <rhffldkt0517@naver.com>
2021-06-06 19:47:22 +0900

    database modify

--- app/src/main/java/com/example/teamproject/MainActivity.kt
+++ app/src/main/java/com/example/teamproject/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.teamproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.teamproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(applicationContext, Example::class.java)
+            startActivity(intent) /// db 예시 화면으로 갑니다.
+
+        }
     }
 }
