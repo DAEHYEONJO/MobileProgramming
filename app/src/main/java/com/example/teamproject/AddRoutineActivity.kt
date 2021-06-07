479634d5156954aa80350d1d8f66a3464b34713a
Wonyeong Choi <coma01155@gmail.com>
2021-06-08 00:17:28 +0900

    feat: add apply and cancel button

--- app/src/main/java/com/example/teamproject/AddRoutineActivity.kt
+++ app/src/main/java/com/example/teamproject/AddRoutineActivity.kt
@@ -1,14 +1,19 @@
 package com.example.teamproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class AddRoutineActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_routine)
+
         init()
     }
     fun max(a:Int, b:Int):Int {
@@ -20,17 +25,30 @@ class AddRoutineActivity : AppCompatActivity() {
         return ret
     }
     private fun init(){
-        var editText = findViewById<EditText>(R.id.repeat)
-        editText.setText("10")
-        var plus_btn = findViewById<ImageButton>(R.id.plus)
-        var minus_btn = findViewById<ImageButton>(R.id.minus)
+        val routine_count = findViewById<EditText>(R.id.routine_count)
+        val plus_btn = findViewById<ImageButton>(R.id.plus)
+        val minus_btn = findViewById<ImageButton>(R.id.minus)
+        val applyBtn = findViewById<Button>(R.id.apply_btn)
+        val cancelBtn = findViewById<Button>(R.id.cancel_btn)
+
+        val intent = Intent(this, CalendarActivity::class.java)
+
+        routine_count.setText("10")
         plus_btn.setOnClickListener {
-            val new_repeat = editText.text.toString().toInt()+1
-            editText.setText(new_repeat.toString())
+            val new_repeat = routine_count.text.toString().toInt()+1
+            routine_count.setText(new_repeat.toString())
         }
         minus_btn.setOnClickListener {
-            val new_repeat = max(editText.text.toString().toInt()-1, 0)
-            editText.setText(new_repeat.toString())
+            val new_repeat = max(routine_count.text.toString().toInt()-1, 0)
+            routine_count.setText(new_repeat.toString())
+        }
+        applyBtn.setOnClickListener {
+            val routine_name = findViewById<TextInputEditText>(R.id.routine_name)
+            // todo: firestore에 저장
+            startActivity(intent)
+        }
+        cancelBtn.setOnClickListener {
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/teamproject/CalendarActivity.kt
+++ app/src/main/java/com/example/teamproject/CalendarActivity.kt
@@ -1,7 +1,9 @@
 package com.example.teamproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,11 @@ class CalendarActivity : AppCompatActivity() {
         init()
     }
     fun init(){
-
+        var fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener{
+            intent = Intent(this, AddRoutineActivity::class.java)
+            // todo: 현재 날짜 넘기기 
+            startActivity(intent)
+        }
     }
 }
