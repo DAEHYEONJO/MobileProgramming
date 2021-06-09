c03b7f2a92bbce61c0de0356a42876cdeec67276
DAEHYEONJO <jdhqpt12@gmail.com>
2021-06-10 01:09:52 +0900

    stt 초기 세팅

--- app/src/main/java/com/example/teamproject/stopwatch/StopWatchFragment.kt
+++ app/src/main/java/com/example/teamproject/stopwatch/StopWatchFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -75,10 +76,24 @@ class StopWatchFragment : Fragment() {
         }
 
         override fun onError(error: Int) {
+            when(error){
+                SpeechRecognizer.ERROR_AUDIO->{Log.e("stt","ERROR_AUDIO")}
+                SpeechRecognizer.ERROR_CLIENT->{Log.e("stt","ERROR_CLIENT")}
+                SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS->{Log.e("stt","ERROR_INSUFFICIENT_PERMISSIONS")}
+                SpeechRecognizer.ERROR_NETWORK->{Log.e("stt","ERROR_NETWORK")}
+                SpeechRecognizer.ERROR_NETWORK_TIMEOUT->{Log.e("stt","ERROR_NETWORK_TIMEOUT")}
+                SpeechRecognizer.ERROR_NO_MATCH->{Log.e("stt","ERROR_NO_MATCH")}
+                SpeechRecognizer.ERROR_RECOGNIZER_BUSY->{Log.e("stt","ERROR_RECOGNIZER_BUSY")}
+                SpeechRecognizer.ERROR_SERVER->{Log.e("stt","ERROR_SERVER")}
+                SpeechRecognizer.ERROR_SPEECH_TIMEOUT->{Log.e("stt","ERROR_SPEECH_TIMEOUT")}
+
+            }
         }
 
         override fun onResults(results: Bundle?) {
-
+            results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.forEach {
+                binding?.sttResult?.text = it.toString()
+            }
         }
     }
 
