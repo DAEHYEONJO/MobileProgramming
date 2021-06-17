29d2caeef8d772c39ede40d3010e07f73614ffad
IslandofDream <rhffldkt0517@naver.com>
2021-06-17 21:23:54 +0900

    현재 날짜 기준으로 연속 운동 기록 출력

--- app/src/main/java/com/example/teamproject/myprofile/Profile.kt
+++ app/src/main/java/com/example/teamproject/myprofile/Profile.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.teamproject.Mydbhelper
 import com.example.teamproject.R
@@ -16,6 +17,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.okhttp.Dispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.util.*
@@ -25,13 +27,13 @@ class Profile : Fragment() {
     var peoplelist:ArrayList<String> = ArrayList()
     lateinit var binding: FragmentProfileBinding
     lateinit var mydbhelper : Mydbhelper
-    lateinit var start_date: LocalDate
     var count : Int = 0
+    var start_date :LocalDate = LocalDate.now()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
         return binding?.root
@@ -40,11 +42,10 @@ class Profile : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mydbhelper = Mydbhelper()
-        start_date = LocalDate.now()
         initData()
         init()
-        countdate()
-
+        countdate(count,start_date)
+        Log.d("asdf", count.toString())
 
 
     }
@@ -52,23 +53,23 @@ class Profile : Fragment() {
     fun init(){
         binding.apply {
             mydbhelper.readname(
-                FirebaseAuth.getInstance().currentUser!!.uid,
-                object : Mydbhelper.MyCallback {
+                    FirebaseAuth.getInstance().currentUser!!.uid,
+                    object : Mydbhelper.MyCallback {
 
-                    override fun onCallback(value: String) {
-                        super.onCallback(value)
-                        nameText.text = value + "님 어서오세요!"
-                    }
-                })
+                        override fun onCallback(value: String) {
+                            super.onCallback(value)
+                            nameText.text = value + "님 어서오세요!"
+                        }
+                    })
 
             mydbhelper.readgender(
-                FirebaseAuth.getInstance().currentUser!!.uid,
-                object : Mydbhelper.MyCallback {
-                    override fun onCallback(value: String) {
-                        super.onCallback(value)
-                        gender.text = value
-                    }
-                })
+                    FirebaseAuth.getInstance().currentUser!!.uid,
+                    object : Mydbhelper.MyCallback {
+                        override fun onCallback(value: String) {
+                            super.onCallback(value)
+                            gender.text = value
+                        }
+                    })
             var num = Random().nextInt(saylist.size)
             sayingtext.text = saylist[num]
             whosaying.text = peoplelist[num]
@@ -77,34 +78,34 @@ class Profile : Fragment() {
     }
 
 
-    fun countdate()  { // 몇일 연속으로 루틴 달성 했는가 보여주는 구문입니다.
-        val account = Account(count.toString(), start_date.toString())
-        CoroutineScope(Dispatchers.IO).launch {
-        FirebaseFirestore.getInstance().collection("Profile")
-            .document(FirebaseAuth.getInstance().currentUser!!.uid).collection("history").get()
-            .addOnSuccessListener {
-                /*
-                    for (document in it){
-                        Log.d("db", document["date"].toString())
-                        Log.d("hi",start_date.toString())
-                        count += 1
-                        start_date.minusDays(1)
-                        if(document["date"].toString() == start_date.toString()) {
+    fun countdate(num :Int, date:LocalDate)  {
+        binding.progressBar2.visibility = View.VISIBLE
+        CoroutineScope(Dispatchers.Main).launch {
+        CoroutineScope(Dispatchers.IO).async {
+            FirebaseFirestore.getInstance().collection("Profile")
+                    .document(FirebaseAuth.getInstance().currentUser!!.uid).collection("history").document(date.toString()).get()
+                    .addOnSuccessListener {Document ->
+                        if(Document.exists()){
+                            start_date = minus(date)
                             count += 1
-                            Log.d("db", document["date"].toString())
-                        }
-                    }*/
-                    binding.routinedays.text = it.size().toString() + " 번"
-                    binding.routinedays.setTextSize(45.0F)
+                            countdate(count,start_date)
+                            }
+                    }
+                    }.await()
+            binding.routinedays.text = count.toString() + "일"
+            binding.progressBar2.visibility =View.GONE
             }
         }
+
+    fun minus(date: LocalDate):LocalDate{
+        return date.minusDays(1)
     }
+
 
     fun readFileScan(scan: Scanner){
         while(scan.hasNextLine()){
             val word = scan.nextLine()
             var list = word.split("–")
-            Log.d("asdf",list.toString())
             saylist.add(list[0])
             peoplelist.add(list[1])
         }
