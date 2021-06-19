6c992be2641e41a8f2374c0163b4a00c563e8328
IslandofDream <rhffldkt0517@naver.com>
2021-06-19 15:39:00 +0900

    로그아웃 버튼 위치 수정 및 db관련 예시파일들 제거

--- app/src/main/java/com/example/teamproject/myprofile/Profile.kt
+++ app/src/main/java/com/example/teamproject/myprofile/Profile.kt
@@ -1,6 +1,8 @@
 package com.example.teamproject.myprofile
 
 import android.accounts.Account
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment
 import com.example.teamproject.Mydbhelper
 import com.example.teamproject.R
 import com.example.teamproject.databinding.FragmentProfileBinding
+import com.example.teamproject.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.okhttp.Dispatcher
@@ -45,6 +48,16 @@ class Profile : Fragment() {
         initData()
         init()
         countdate(count,start_date)
+
+        binding.logoutBtn.setOnClickListener {  //로그하웃 하기
+            FirebaseAuth.getInstance().signOut()
+            val i = Intent(activity as Activity, LoginActivity::class.java)
+            i.flags= Intent.FLAG_ACTIVITY_CLEAR_TASK
+            i.flags= Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(i)
+            activity?.finish()
+        }
+
 
     }
 
